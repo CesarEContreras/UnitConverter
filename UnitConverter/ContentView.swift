7c7599d04cdc0d1b409bc90e594356db3ca6121d
Cesar Contreras <cesar.contrerasm@outlook.com>
2020-06-10 19:25:55 -0400

    Initial Commit

---
+++ UnitConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConverter
+//
+//  Created by Cesar Contreras on 6/10/20.
+//  Copyright © 2020 Cesar Contreras. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
