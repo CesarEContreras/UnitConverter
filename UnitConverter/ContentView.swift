59880aad04a2d2bf46b82cba8f8fcc76e3716092
Cesar Contreras <cesar.contrerasm@outlook.com>
2020-06-11 15:56:58 -0400

    Adds computed property to calculate conversion

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -15,6 +15,50 @@ struct ContentView: View {
     
     let distanceUnits = ["meters", "kilometers", "feet", "yard", "miles"]
     
+    var outputNumber: Double {
+        let value = Double(inputNumber) ?? 0
+        let fromUnit = distanceUnits[inputUnit]
+        let toUnit = distanceUnits[outputUnit]
+        
+        var outputValue: Double = 0
+        var valueInMeter: Measurement<UnitLength>
+        
+        
+        switch fromUnit {
+        case "kilometers":
+            let distanceKm = Measurement(value: value, unit: UnitLength.kilometers)
+            valueInMeter = distanceKm.converted(to: UnitLength.meters)
+        case "feet":
+            let distanceFt = Measurement(value: value, unit: UnitLength.feet)
+            valueInMeter = distanceFt.converted(to: UnitLength.meters)
+        case "yard":
+            let distanceYard = Measurement(value: value, unit: UnitLength.yards)
+            valueInMeter = distanceYard.converted(to: UnitLength.meters)
+        case "miles":
+            let distanceMiles = Measurement(value: value, unit: UnitLength.miles)
+            valueInMeter = distanceMiles.converted(to: UnitLength.meters)
+        default:
+            valueInMeter = Measurement(value: value, unit: UnitLength.meters)
+        }
+        
+        switch toUnit {
+        case "meters":
+            outputValue = valueInMeter.value
+        case "kilometers":
+            outputValue = valueInMeter.converted(to: UnitLength.kilometers).value
+        case "feet":
+            outputValue = valueInMeter.converted(to: UnitLength.feet).value
+        case "yard":
+            outputValue = valueInMeter.converted(to: UnitLength.yards).value
+        case "miles":
+            outputValue = valueInMeter.converted(to: UnitLength.miles).value
+        default:
+            outputValue = 0
+        }
+        
+        return outputValue
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -30,13 +74,17 @@ struct ContentView: View {
                     .pickerStyle(SegmentedPickerStyle())
                 }
                 
-                Section(header: Text("Output  Measure")) {
+                Section(header: Text("Output  Unit")) {
                     Picker("Output Unit", selection: $outputUnit) {
                         ForEach(0 ..< distanceUnits.count) {
                             Text("\(self.distanceUnits[$0])")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Output Measure")) {
+                    Text("\(outputNumber, specifier: "%.2f")")
                 }
             }
             .navigationBarTitle("Unit Converter")
