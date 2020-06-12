49a595845dd9403624acd126d424e5166a5a6417
Cesar Contreras <cesar.contrerasm@outlook.com>
2020-06-11 20:53:14 -0400

    Updating prebuild file

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,4 +1,4 @@
 echo "Injecting secrets..."
 echo "Updating iOS secret"
-echo $IOS_SECRET | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/UnitConverter/AppCenter-Config.plist"
+echo $IOS_SECRET | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/Glitz/AppCenter-Config.plist"
 echo "Finished injecting secrets."
