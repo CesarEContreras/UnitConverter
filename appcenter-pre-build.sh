70c3979edec08130484b86ac83cbe747542d2f53
Cesar Contreras <cesar.contrerasm@outlook.com>
2020-06-11 21:04:59 -0400

    Updating sh file

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,4 +1,9 @@
 echo "Injecting secrets..."
 echo "Updating iOS secret"
+
+Get-ChildItem -Path $APPCENTER_SOURCE_DIRECTORY/ios -Filter *.js -Recurse -File -Name| ForEach-Object {
+    $_.FullName
+}
+
 echo $IOS_SECRET | base64 --decode > "$APPCENTER_SOURCE_DIRECTORY/ios/Glitz/AppCenter-Config.plist"
 echo "Finished injecting secrets."
